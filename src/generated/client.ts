04db099a7e0f2aa0fcc615edc145562fb53273d2
FelixManFongCheung <manfongcheung74@gmail.com>
2025-12-04 19:07:07 +0100

    re generated prisma

--- src/generated/client.ts
+++ src/generated/client.ts
@@ -10,18 +10,17 @@
  * 🟢 You can import this file directly.
  */
 
-import * as process from 'node:process'
 import * as path from 'node:path'
 import { fileURLToPath } from 'node:url'
 globalThis['__dirname'] = path.dirname(fileURLToPath(import.meta.url))
 
 import * as runtime from "@prisma/client/runtime/client"
-import * as $Enums from "./enums"
 import * as $Class from "./internal/class"
 import * as Prisma from "./internal/prismaNamespace"
 
-export * as $Enums from './enums'
 export * from "./enums"
+export * as $Enums from './enums'
+export { Prisma }
 /**
  * ## Prisma Client
  * 
@@ -37,7 +36,6 @@ export * from "./enums"
  */
 export const PrismaClient = $Class.getPrismaClientClass()
 export type PrismaClient<LogOpts extends Prisma.LogLevel = never, OmitOpts extends Prisma.PrismaClientOptions["omit"] = Prisma.PrismaClientOptions["omit"], ExtArgs extends runtime.Types.Extensions.InternalArgs = runtime.Types.Extensions.DefaultArgs> = $Class.PrismaClient<LogOpts, OmitOpts, ExtArgs>
-export { Prisma }
 
 /**
  * Model Asset
