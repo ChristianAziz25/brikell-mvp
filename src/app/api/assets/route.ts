33332e8670871eb8a85e5e758963a1f207508931
FelixManFongCheung <manfongcheung74@gmail.com>
2025-12-09 21:12:04 +0100

    set my asset back to minimal

--- src/app/api/assets/route.ts
+++ src/app/api/assets/route.ts
@@ -1,10 +1,14 @@
-import { getAllAssetsNames } from "@/lib/prisma/models/asset";
+import { getAllAssets } from "@/lib/prisma/models/asset";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const assets = await getAllAssetsNames();
-    return NextResponse.json(assets.map((asset) => asset.name));
+    const assets = await getAllAssets();
+    const assetsNames = assets.map((asset) => ({
+      id: asset.id,
+      name: asset.name,
+    }));
+    return NextResponse.json(assetsNames);
   } catch (error) {
     console.error("Error fetching assets:", error);
     return NextResponse.json(
